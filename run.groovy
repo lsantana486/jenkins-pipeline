3943c087001ad8b5cb2a74b916020a88f1944cfd
Luis Santana <l.santana@globant.com>
2021-02-04 12:51:57 -0300

    Test http client

--- run.groovy
+++ run.groovy
@@ -1,19 +1,9 @@
-@Grapes(
-    @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
-)
-
-import groovyx.net.http.HTTPBuilder
-import static groovyx.net.http.ContentType.*
-import static groovyx.net.http.Method.*
-
 node('master') {
   checkout scm
-  def http = new HTTPBuilder("http://httpbin.org/post'")
-  http.request(POST, JSON ) { req ->
-    body = []
-    response.success = { resp, reader ->
-        println "$resp.statusLine   Respond rec"
-
-    }
-  }
+  def patchOrg = """
+    {"test": "test"}
+  """
+  def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: patchOrg, url: 'http://httpbin.org/post'
+  println('Status: '+response.status)
+  println('Response: '+response.content)
 }
