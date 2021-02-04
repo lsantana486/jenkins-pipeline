16a5c51ac71be1bb52edd8b00d3d7c36d7c603cb
Luis Santana <l.santana@globant.com>
2021-02-04 11:35:40 -0300

    Test http client

--- run.groovy
+++ run.groovy
@@ -1,3 +1,9 @@
 node('master') {
-  sh "echo 'Hello'"
+  def client = evaluate(new File('./RestClient.groovy'))
+
+  def resp = client.post('http://httpbin.org/post', [
+    jobName       : env.JOB_NAME
+  ])
+  assert resp.status == 201
+  println "${resp}"
 }
