39c4b728e54a944e39c2348e7cb619acdcd35578
Luis Santana <l.santana@globant.com>
2021-02-15 12:36:06 -0300

    Testing docker exec

--- docker_exec.groovy
+++ docker_exec.groovy
@@ -1,6 +1,7 @@
 node('master') {
   checkout scm
-  docker.image("alpine:latest").inside('--entrypoint "" -u 0:0') {
+  def img = docker.build("jkalpine")
+  img.inside('--entrypoint "" -u 0:0') {
     sh "ls -la /Users/l.santana/.jenkins/workspace/POC"
     sh "touch /Users/l.santana/.jenkins/workspace/POC/test.test"
     sh "sleep 600"
