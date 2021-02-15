5c55e6db2fbd3607abd0dadf0b505759c6b43ab7
Luis Santana <l.santana@globant.com>
2021-02-15 11:54:05 -0300

    Testing docker exec

--- docker_exec.groovy
+++ docker_exec.groovy
@@ -1,8 +1,7 @@
 node('master') {
   checkout scm
   docker.image("alpine:latest").inside('--entrypoint "" -u 0:0') {
-    sh "ls -la /"
-    sh "mkdir -p /test"
-    sh "sleep 600"
+    sh "ls -la /Users/l.santana/.jenkins/workspace/POC"
+    sh "touch /Users/l.santana/.jenkins/workspace/POC/test.test"
   }
 } 
