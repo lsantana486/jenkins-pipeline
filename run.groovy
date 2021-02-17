4c1f33a93404802372b904ae52e9a0e65319ae66
Luis Santana <l.santana@globant.com>
2021-02-17 13:42:50 -0300

    Updating run

--- run.groovy
+++ run.groovy
@@ -1,17 +1,6 @@
 node('master') {
   wrap([$class: 'TimestamperBuildWrapper']) {
-    checkout scm
-    checkout ([
-        $class: 'GitSCM',
-        branches: [[name: "${params.BRANCH}"]],
-        extensions: [
-          [$class: 'PruneStaleBranch'],
-          [$class: 'CleanCheckout'],
-          [$class: 'UserIdentity', email: 'dev@null', name: 'devnull'],
-          [$class: 'CloneOption', noTags: false, reference: '', shallow: true, depth: 20]
-        ],
-        userRemoteConfigs: [[url: "${params.GIT_URL}"]]
-    ])
+    checkout scm 
     load "${params.TEMPLATE}"
   } 
 }
