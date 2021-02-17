04b5c69d2375a88b5c243126c28b87c7b7c8b3c6
Luis Santana <l.santana@globant.com>
2021-02-17 13:20:06 -0300

    Updating structure

---
+++ http/http_client.groovy
@@ -0,0 +1,20 @@
+import groovy.json.JsonSlurper;
+import org.apache.commons.codec.binary.Base64;
+
+node('master') {
+  checkout scm
+  def get = new URL('http://localhost:8081/service/rest/v1/search?repository=wm-maven&name=my-app&version=1.0.2')
+  def authEncBytes = Base64.encodeBase64("admin:nexus@local".getBytes())
+  def authStringEnc = new String(authEncBytes)
+  def conn = get.openConnection()
+  conn.setRequestProperty("Authorization", "Basic ${authStringEnc}")
+  conn.setRequestProperty("Accept", "application/json")
+  conn.setRequestMethod("GET")
+  if (conn.getResponseCode().equals(200)){
+    def slurper = new groovy.json.JsonSlurper()
+    def result = slurper.parseText(conn.getInputStream().getText());
+    println "RESPONSE: ${result.items}"
+  } else {
+    println "RESPONSE: error"
+  }
+}
--- run.groovy
+++ run.groovy
@@ -1,20 +1,17 @@
-import groovy.json.JsonSlurper;
-import org.apache.commons.codec.binary.Base64;
-
 node('master') {
-  checkout scm
-  def get = new URL('http://localhost:8081/service/rest/v1/search?repository=wm-maven&name=my-app&version=1.0.2')
-  def authEncBytes = Base64.encodeBase64("admin:nexus@local".getBytes())
-  def authStringEnc = new String(authEncBytes)
-  def conn = get.openConnection()
-  conn.setRequestProperty("Authorization", "Basic ${authStringEnc}")
-  conn.setRequestProperty("Accept", "application/json")
-  conn.setRequestMethod("GET")
-  if (conn.getResponseCode().equals(200)){
-    def slurper = new groovy.json.JsonSlurper()
-    def result = slurper.parseText(conn.getInputStream().getText());
-    println "RESPONSE: ${result.items}"
-  } else {
-    println "RESPONSE: error"
-  }
+  wrap([$class: 'TimestamperBuildWrapper']) {
+    checkout scm
+    checkout ([
+        $class: 'GitSCM',
+        branches: [[name: "${p.BRANCH ? p.BRANCH : 'main'}"]],
+        extensions: [
+          [$class: 'PruneStaleBranch'],
+          [$class: 'CleanCheckout'],
+          [$class: 'UserIdentity', email: 'dev@null', name: 'devnull'],
+          [$class: 'CloneOption', noTags: false, reference: '', shallow: true, depth: 20]
+        ],
+        userRemoteConfigs: [[url: "${p.GIT_URL}"]]
+    ])
+    load "${p.TEMPLATE ? p.TEMPLATE : 'test.groovy'}"
+  } 
 }
