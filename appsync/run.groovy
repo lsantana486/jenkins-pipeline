234c0c3b7831ec607f5f51dc66f40a6ce7998f7f
Luis Santana <l.santana@globant.com>
2021-02-17 17:51:22 -0300

    Test amplify headless

---
+++ appsync/run.groovy
@@ -0,0 +1,42 @@
+checkout ([
+  $class: 'GitSCM',
+  branches: [[name: "${params.BRANCH}"]],
+  extensions: [
+    [$class: 'PruneStaleBranch'],
+    [$class: 'CleanCheckout'],
+    [$class: 'UserIdentity', email: 'dev@null', name: 'devnull'],
+    [$class: 'CloneOption', noTags: false, reference: '', shallow: true, depth: 20]
+  ],
+  userRemoteConfigs: [[url: "${params.GIT_URL}"]]
+])
+withAWS(profile:'amplify-datalegion') {
+  sh """\
+    AMPLIFY='{ \
+      \"projectName\":\"amplifyheadlessci\",\
+      \"envName\":\"dev\",\
+      \"defaultEditor\":\"code\"\
+    }' \
+    APP='{ \
+      \"frontend\":\"javascript\",\
+      \"framework\":\"none\",\
+      \"config\":{\
+        \"SourceDir\":\"src\",\
+        \"DistributionDir\":\"dist\",\
+        \"BuildCommand\":\"npm run-script build\",\
+        \"StartCommand\":\"npm run-script start\"\
+      }\
+    }' \
+    PROVIDERS='{\
+      \"awscloudformation\":{
+        \"configlevel\":\"project\",\
+        \"useProfile\":true,\
+        \"profileName\":\"amplify-datalegion\"\
+      }\
+    }'\
+    amplify init \
+    --amplify \$AMPLIFY \
+    --frontend \$APP \
+    --providers \$PROVIDERS \
+    --yes \
+  """
+}
--- test.groovy
+++ test.groovy
@@ -1,12 +1,1 @@
-checkout ([
-  $class: 'GitSCM',
-  branches: [[name: "${params.BRANCH}"]],
-  extensions: [
-    [$class: 'PruneStaleBranch'],
-    [$class: 'CleanCheckout'],
-    [$class: 'UserIdentity', email: 'dev@null', name: 'devnull'],
-    [$class: 'CloneOption', noTags: false, reference: '', shallow: true, depth: 20]
-  ],
-  userRemoteConfigs: [[url: "${params.GIT_URL}"]]
-])
 sh "echo test"
