b37f37f49b29f5401a9d56cf327933a9f5acc130
Luis Santana <l.santana@globant.com>
2021-02-18 15:09:02 -0300

    Test bash

--- bash/run.groovy
+++ bash/run.groovy
@@ -1,2 +1,2 @@
 def test = sh(script: 'for rs in \$(find ./bash/ -type f \\( -iname \"*.ruleset\" ! -iname \"*.strict.ruleset\" \\)); do OUTPUT=\"\$(echo $rs)\n\$OUTPUT\"; done', returnStdout: true).trim()
-
+println "TEST: ${test}"
