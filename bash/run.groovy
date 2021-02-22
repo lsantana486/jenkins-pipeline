dacbd8b4fadcbddb1032fac6acc898eb460a34ba
Luis Santana <l.santana@globant.com>
2021-02-22 17:37:16 -0300

    Test bg bash

--- bash/run.groovy
+++ bash/run.groovy
@@ -1,2 +1,8 @@
-def test = sh(script: 'for rs in \$(find ./bash/ -type f \\( -iname \"*.ruleset\" ! -iname \"*.strict.ruleset\" \\)); do OUTPUT=\"\$(cat $rs)\n\$OUTPUT\"; done && echo $OUTPUT', returnStdout: true).trim()
+def test = sh(script: 'for rs in \$(find ./bash/ -type f \\( -iname \"*.ruleset\" ! -iname \"*.strict.ruleset\" \\)); do OUTPUT=\"\$(cat $rs)\n\$OUTPUT\"; done && echo $OUTPUT', returnStdout: true)
 println "TEST: ${test}"
+
+def cmd = "sleep 10 && echo test && exit 0"
+def bPID = sh(script: "eval ${cmd} 2>&1 > output.log &; echo $!", returnStdout: true)
+println "PID: ${bPID}"
+sh "wait ${bPID} && echo $?"
+
