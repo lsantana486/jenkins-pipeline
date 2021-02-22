18d546cb6253a586a0532299c934fb0821c1a1c9
Luis Santana <l.santana@globant.com>
2021-02-22 17:56:32 -0300

    Test bg bash

--- bash/run.groovy
+++ bash/run.groovy
@@ -2,7 +2,7 @@ def test = sh(script: 'for rs in \$(find ./bash/ -type f \\( -iname \"*.ruleset\
 println "TEST: ${test}"
 
 def cmd = "sleep 10 && echo test && exit 0"
-def bPID = sh(script: "eval ${cmd} 2>&1 > output.log &; echo \$!", returnStdout: true)
+def bPID = sh(script: "bash bg-process.sh \"${cmd}\"", returnStdout: true)
 println "PID: ${bPID}"
 sh "wait ${bPID} && echo \$?"
 
