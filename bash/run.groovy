1d0dcdd2e3203882e03c3516af3ff2531957decb
Luis Santana <l.santana@globant.com>
2021-02-26 15:20:50 -0300

    Test output bash

--- bash/run.groovy
+++ bash/run.groovy
@@ -11,3 +11,28 @@ for(ruleset in readJSON(text: test)) {
 }
 
 println "OUTPUT: ${output}"
+
+test = sh(script: 'cat ./bash/output.txt', returnStdout: true).trim()
+def splitTest = test.tokenize('\n\n')
+println splitTest
+splitTest = splitTest.unique()
+println splitTest
+def newTest = []
+for (st in splitTest) {
+    //println st
+    def matcherA = st =~ /^\[(?<policy>.*)\] failed because \[(?<rscr>.*)\] is(?<sep>.*) \[(?<value>.*)\] (?<msg>.*)$/
+    if (matcherA.matches()) {
+        //println matcherA.group("policy")
+        //println matcherA.group("rscr")
+        //println matcherA.group("value")
+        //println matcherA.group("msg")
+        newTest.add(matcherA.replaceAll('(${policy}) failed because (${rscr}) is${sep} (${value}) ${msg}'))
+    }
+    def matcherB = st =~ /^\[(?<policy>.*)\] failed because it does not contain the required property of \[(?<property>.*)\]$/
+    if (matcherB.matches()) {
+        //println matcherB.group("policy")
+        //println matcherB.group("property")
+        newTest.add(matcherB.replaceAll('(${policy}) failed because it does not contain the required property of (${property})'))
+    }
+}
+println newTest.join('\n')
