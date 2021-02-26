fb91b2ea2399a16a29373ccb2181880df0bfd356
Luis Santana <l.santana@globant.com>
2021-02-26 10:08:31 -0300

    Test output bash

--- bash/run.groovy
+++ bash/run.groovy
@@ -1,26 +1,11 @@
-//def test = sh(script: 'for rs in \$(find ./bash/ -type f \\( -iname \"*.ruleset\" ! -iname \"*.strict.ruleset\" \\)); do OUTPUT=\"\$(cat $rs)\n\$OUTPUT\"; done && echo $OUTPUT', returnStdout: true)
-//println "TEST: ${test}"
+// Test process
+//println "$WORKSPACE/bash/bg-process.sh".execute().text
 
-//def cmd = "sleep 10 && echo test && exit 0"
-//def bPID = sh(script: "bash \$WORKSPACE/bash/bg-process.sh \"${cmd}\"", returnStdout: true)
-//println "PID: ${bPID}"
-//sh "wait ${bPID} && echo \$?"
-//def cmd = 'sleep 10 && echo test && exit 0'.execute()
-//while (true) {
-//  def breakCmd = sh(script: '[ -f stop ] && echo 1 || echo 0', returnStdout: true).trim().equals("1")
-//  if (breakCmd) {
-//    cmd.destroy()
-//    break
-//  } else {
-//    println "Checking..."
-//    sleep(5)
-//  }
-//}
 
-//def exitValue = cmd.exitValue()
-//println "${exitValue}"
+def test = sh(script: 'find ./bash/ -type f \\( -iname \"*.ruleset\" ! -iname \"*.strict.ruleset\" \\) | jq -R . | jq -s .', returnStdout: true)
+println "TEST: ${test}"
 
-//cmd.in.eachLine { println it }
-//cmd.err.eachLine { println it }
-//println "${cmd.text}"
-println "\$WORKSPACE/bash/bg-process.sh".execute().text
+for(ruleset in readJSON(text: test)) {
+  sh "cat ${ruleset}"
+}
+
