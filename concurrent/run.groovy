ede058d4cba259905784eb893c9ec2ed6a67c88e
Luis Santana <l.santana@globant.com>
2021-06-15 20:50:23 -0400

    ci(CONCURRENT): Test parallel

--- concurrent/run.groovy
+++ concurrent/run.groovy
@@ -5,14 +5,69 @@ tasksSettings = [
   ],
   [
     name: "taskB",
-    exec: "sleep 3"
+    exec: "sleep 4"
   ],
   [
     name: "taskC",
-    exec: "sleep 1"
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskD",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskE",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskF",
+    exec: "sleep 4"
+  ]
+  [
+    name: "taskG",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskH",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskI",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskJ",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskK",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskL",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskM",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskN",
+    exec: "sleep 4"
+  ]
+  [
+    name: "taskO",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskP",
+    exec: "sleep 4"
+  ],
+  [
+    name: "taskQ",
+    exec: "sleep 4"
   ]
 ]
-def barrier = createBarrier count: tasksSettings.size();
 echo "Start pipeline"
 def tasks = [:]
 for (taskSettings in tasksSettings) {
@@ -22,7 +77,8 @@ for (taskSettings in tasksSettings) {
   }
 }
 parallel tasks
-/*parallel(
+/*def barrier = createBarrier count: tasksSettings.size();
+parallel(
     taskA: {
       awaitBarrier (barrier){
         echo "Start taskA"
