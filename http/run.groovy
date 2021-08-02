39efc25ec6178d22b6ddb5bb7e2ae0fcd995824a
Luis Santana <l.santana@globant.com>
2021-08-02 17:08:03 -0400

    ci(HTTP): Testing elk

--- http/run.groovy
+++ http/run.groovy
@@ -4,6 +4,7 @@ import com.amazonaws.auth.AWS4Signer
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.http.HttpMethodName
 import com.amazonaws.AmazonServiceException
+import com.amazonaws.AmazonWebServiceResponse
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.DefaultRequest
 import com.amazonaws.Request
@@ -13,6 +14,20 @@ import com.amazonaws.http.ExecutionContext
 import com.amazonaws.http.HttpResponseHandler
 import groovy.json.JsonSlurper
 import groovy.io.FileType
+
+public final class DummyHandler<T> implements HttpResponseHandler<T> {
+    private final T preCannedResponse;
+    public DummyHandler(T preCannedResponse) { this.preCannedResponse = preCannedResponse; }
+
+    @Override
+    public T handle(HttpResponse response) throws Exception {
+        System.out.println(IOUtils.toString(response.getContent()));
+        return preCannedResponse;
+    }
+
+    @Override
+    public boolean needsConnectionLeftOpen() { return false; }
+}
 
 def payload = '''{ "index": { "_index": "mstack360-poc-notes", "_id": "6e568f1e-5235-4d6b-b3c9-684f33b31ed5" } }
 {"name":"wm-demo-lib","type":"note","shortDescription":"This is a test","longDescription":"This is a test using ELK to save metadata for wm-demo-lib","kind":"BUILD","build":{"builderVersion":"1.0.0","signature":{"publicKey":"","signature":"Z3JhZmVhcw==","keyId":"04A49FE3","keyType":"PGPKEY"}},"id":"6e568f1e-5235-4d6b-b3c9-684f33b31ed5","@timestamp":"2021-08-01T01:00:00.000Z"}'''
@@ -40,7 +55,7 @@ withAWS(credentials: "awspoc", duration: 900, roleSessionName: "jenkins-session"
             .requestExecutionBuilder()
             .executionContext(new ExecutionContext(true))
             .request(request)
-            .errorResponseHandler(new HttpResponseHandler<AmazonServiceException>())
-            .execute(new HttpResponseHandler<T>());
+            .errorResponseHandler(new DummyHandler<>(new AmazonServiceException("oops")))
+            .execute(new DummyHandler<>(new AmazonWebServiceResponse<Void>()));
     awsResponse = response.getAwsResponse();
 }
