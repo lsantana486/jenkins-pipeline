1292eb02fa1ba7933928cef179b144f7b0033166
Luis Santana <l.santana@globant.com>
2021-08-06 15:29:50 -0400

    ci(TEST): Testing dirs

--- test.groovy
+++ test.groovy
@@ -1,10 +1,31 @@
 sh "echo test"
 
-def files = findFiles() 
-files.each{ f -> 
-  if(f.directory) {
-    dir(f.name) {
-      sh "ls -la"
+def getMStack360Runtime(recursive=true) {
+    def runtime = "unknown"
+    def tmpRuntime = "unknown"
+    if(fileExists('Dockerfile')) {
+        runtime = "docker"
+            
+    } else if(fileExists('package.json')) {
+        runtime = "nodejs"
+                
+    } else if(fileExists('pom.xml')) {
+        runtime = "java"
+        
+    } else {
+        def items = findFiles()
+        for (item in items) {
+            if(item.directory) {
+                dir(item.name) {
+                  tmpRuntime = getMStack360Runtime(false)
+                  if(!tmpRuntime.equals("unknown")) {
+                    runtime = tmpRuntime
+                  }
+                }
+            }
+        }
     }
-  }
+    return runtime
 }
+
+println "${getMStack360Runtime()}"
