42f71f2d1b3939f2087a99260320110472e67922
Luis Santana <l.santana@globant.com>
2021-08-13 09:36:49 -0400

    ci(HTTP): Testing yaml

--- http/run.groovy
+++ http/run.groovy
@@ -1,6 +1,15 @@
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput 
+
 JsonSlurper jsonSlurper = new JsonSlurper()
 def connection = new URL('http://httpbin.org/get').openConnection()
 def response = connection.getContent().newReader()
 def apiResponse = jsonSlurper.parse(response)
 println "${apiResponse}"
+def output = JsonOutput.toJson(apiResponse)
+println "${output}"
+
+def response = 'http://httpbin.org/get'.toURL().getText()
+def result = new JsonSlurper().parseText(response)
+def content = new String(result.content.decodeBase64())
+def yamlConfig = readYaml(text: content)
