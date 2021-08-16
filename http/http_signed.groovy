a15a9454d199effcb54c355915fb01dab1c8e7ef
Luis Santana <l.santana@globant.com>
2021-08-16 19:07:32 -0400

    ci(ELK): Testing groovy aws sigv4

--- http/http_signed.groovy
+++ http/http_signed.groovy
@@ -43,7 +43,7 @@ withAWS(credentials: "awspoc", duration: 900, roleSessionName: "jenkins-session"
 
 @NonCPS
 def sendRequest(user, pass) {
-  def endpoint = "search-lsantana-sgppskvlgamskg4saasbs2rpca.us-east-1.es.amazonaws.com".toString()
+  def endpoint = "search-lsantana-dnx3pxw56bwwkvija6naqyoz24.us-east-2.es.amazonaws.com".toString()
   def payload = '''{ "index": { "_index": "mstack360-poc-notes", "_id": "6e568f1e-5235-4d6b-b3c9-684f33b31ed5" } }
 {"name":"wm-demo-lib","type":"note","shortDescription":"This is a test","longDescription":"This is a test using ELK to save metadata for wm-demo-lib","kind":"BUILD","build":{"builderVersion":"1.0.0","signature":{"publicKey":"","signature":"Z3JhZmVhcw==","keyId":"04A49FE3","keyType":"PGPKEY"}},"id":"6e568f1e-5235-4d6b-b3c9-684f33b31ed5","@timestamp":"2021-08-01T01:00:00.000Z"}'''.toString()
   def request = new DefaultRequest<Void>("es".toString())
@@ -53,7 +53,7 @@ def sendRequest(user, pass) {
   request.setContent(new ByteArrayInputStream(body))
   request.setHeaders(
     [
-        'Host': 'search-ps-logs-preprod-applogs-ou4fjmihj4tkdmcuryv6pnxtuy.us-east-1.es.amazonaws.com'.toString(),
+        'Host': "${endpoint}".toString(),
         'Content-Type': 'application/json'.toString(),
         'Content-Length': "${body.size()}".toString()
     ]
